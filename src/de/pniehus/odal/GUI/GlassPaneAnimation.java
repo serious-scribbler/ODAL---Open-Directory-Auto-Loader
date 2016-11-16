4ab67de7aafb211ba06c4cc524d2ef1fad9ea881
Phil Niehus <contact@pniehus.de>
2016-11-16 17:37:39 +0100

    Added method to load all png images from a directory

--- src/de/pniehus/odal/GUI/GlassPaneAnimation.java
+++ src/de/pniehus/odal/GUI/GlassPaneAnimation.java
@@ -1,6 +1,13 @@
 package de.pniehus.odal.GUI;
 
 import java.awt.Image;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
 
 /**
  * This class can be used to display animations on {@link AnimatedGlassPane}
@@ -23,5 +30,28 @@ public class GlassPaneAnimation {
 	
 	public Image[] getImages(){
 		return images;
+	}
+	
+	/**
+	 * Loads all png images from the specified directory to an image array
+	 * @param dir
+	 * @return
+	 * @throws IOException gets thrown when the method is unable to read the files
+	 */
+	public static Image[] readImagesFromDirectory(File dir) throws IOException{
+		if(dir == null || !dir.isDirectory() || !dir.canRead()) throw new IOException("invalid input directory");
+		List<Image> imgs = new ArrayList<Image>();
+		
+		for(File f : dir.listFiles(new FileFilter() {
+			
+			@Override
+			public boolean accept(File pathname) {
+				return pathname.getAbsolutePath().endsWith(".png");
+			}
+		})){
+			imgs.add(ImageIO.read(f));
+		}
+		Image[] loaded;
+		return imgs.toArray(new Image[]{}); // Creates a new array, because the give one is to small
 	}
 }
