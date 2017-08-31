47e523d188cea8203aad14c50f31c38859540b3f
Phil Niehus <contact@pniehus.de>
2017-08-31 23:20:47 +0200

    Added support for unsigned certificates

--- src/main/java/de/pniehus/odal/App.java
+++ src/main/java/de/pniehus/odal/App.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import de.pniehus.odal.GUI.OdalGui;
 import de.pniehus.odal.utils.Filter;
 import de.pniehus.odal.utils.filters.BlacklistFilter;
@@ -15,16 +20,36 @@ import de.pniehus.odal.utils.filters.RegexFilter;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws IOException
-    {
-    	List<Filter> filters = new ArrayList<Filter>();
+public class App {
+	public static void main(String[] args) throws IOException {
+		unsignedSSLSetup();
+		List<Filter> filters = new ArrayList<Filter>();
 		filters.add(new RegexFilter());
 		filters.add(new FileTypeFilter());
 		filters.add(new KeywordFilter());
 		filters.add(new BlacklistFilter());
 		@SuppressWarnings("unused")
 		OdalGui ogui = new OdalGui(args, filters);
-    }
+	}
+
+	public static void unsignedSSLSetup() {
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+
+			public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+			}
+
+			public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+			}
+		} };
+		try{
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		} catch(Exception e){
+			// TODO Handle
+		}
+	}
 }
