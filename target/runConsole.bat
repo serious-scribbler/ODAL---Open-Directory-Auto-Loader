d4261fa5fe1062e5cffb34e9835789bb25163d9f
Phil Niehus <contact@pniehus.de>
2018-06-01 17:39:45 +0200

    Created batch file for faster testing

---
+++ target/runConsole.bat
@@ -0,0 +1,4 @@
+java -jar ODAL-0.1.1-SNAPSHOT-jar-with-dependencies.jar -w -url file:///C:/Users/philn/Documents/Projects/ -select-all -p Test
+echo "Test finnished, press any key to delete logs"
+pause
+rm -R logs
